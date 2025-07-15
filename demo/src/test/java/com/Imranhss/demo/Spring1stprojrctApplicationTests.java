73f49b2e01827264f54dacb55cb4fd8829f835d5
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-07-15 19:07:04 +0600

    add

---
+++ demo/src/test/java/com/Imranhss/demo/Spring1stprojrctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Imranhss.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1stprojrctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
